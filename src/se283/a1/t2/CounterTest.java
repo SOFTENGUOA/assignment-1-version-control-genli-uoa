1bc9235e905e86f6920a99e48e77ed4eff695c7b
Gen Li <gli@aucklanduni.ac.nz>
2021-08-13 22:09:31 +0900

    test
    Decomposed and implemented tests in class CounterTest.
    
    - Decomposed test testIncrement to tests testIncrementFromNegativeToNegative, testIncrementFromNegativeToZero, testIncrementFromZeroToPositive, and testIncrementFromPositiveToPositive, and implemented them.
    - Decomposed test testDecrement to tests testDecrementFromPositiveToPositive, testDecrementFromPositiveToZero, testDecrementFromZeroToNegative, and testDecrementFromNegativeToNegative, and implemented them.
    - Decomposed test testReset to tests testResetFromNegative, testResetFromZero, and testResetFromPositive, and implemented them.
    - Decomposed test testMultiplyBy to tests testMultiplyByNegativeAndNegative, testMultiplyByNegativeAndZero, testMultiplyByNegativeAndPositive, testMultiplyByZeroAndNegative, testMultiplyByZeroAndZero, testMultiplyByZeroAndPositive, testMultiplyByPositiveAndNegative, testMultiplyByPositiveAndZero, and testMultiplyByPositiveAndPositive, and implemented them.

--- src/se283/a1/t2/CounterTest.java
+++ src/se283/a1/t2/CounterTest.java
@@ -6,6 +6,7 @@ package se283.a1.t2;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import se283.a1.t1.Counter;
 
 /**
  * Test class for Counter class
@@ -19,32 +20,259 @@ class CounterTest {
 	 * Test method for {@link se283.a1.t1.Counter#increment()}.
 	 */
 	@Test
-	void testIncrement() {
-		fail("Not yet implemented");
+	void testIncrementFromNegativeToNegative() {
+        Counter counter = new Counter();
+        counter._ctr = -2;
+ 
+        counter.increment();
+
+        assertEquals(-1, counter._ctr, "The counter's value is -2 before method increment executes, so the value should be -1 after it has executed.");
+    }
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#increment()}.
+	 */
+	@Test
+	void testIncrementFromNegativeToZero() {
+        Counter counter = new Counter();
+        counter._ctr = -1;
+ 
+        counter.increment();
+
+        assertEquals(0, counter._ctr, "The counter's value is -1 before method increment executes, so the value should be 0 after it has executed.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#increment()}.
+	 */
+	@Test
+	void testIncrementFromZeroToPositive() {
+        Counter counter = new Counter();
+        counter._ctr = 0;
+ 
+        counter.increment();
+
+        assertEquals(1, counter._ctr, "The counter's value is 0 before method increment executes, so the value should be 1 after it has executed.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#increment()}.
+	 */
+	@Test
+	void testIncrementFromPositiveToPositive() {
+        Counter counter = new Counter();
+        counter._ctr = 1;
+ 
+        counter.increment();
+
+        assertEquals(2, counter._ctr, "The counter's value is 1 before method increment executes, so the value should be 2 after it has executed.");
 	}
 
 	/**
 	 * Test method for {@link se283.a1.t1.Counter#decrement()}.
 	 */
 	@Test
-	void testDecrement() {
-		fail("Not yet implemented");
+	void testDecrementFromPositiveToPositive() {
+		Counter counter = new Counter();
+        counter._ctr = 2;
+       
+        counter.decrement();
+
+        assertEquals(1, counter._ctr, "The counter's value is 2 before method decrement executes, so the value should be 1 after it has executed.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#decrement()}.
+	 */
+	@Test
+	void testDecrementFromPositiveToZero() {
+		Counter counter = new Counter();
+        counter._ctr = 1;
+       
+        counter.decrement();
+
+        assertEquals(0, counter._ctr, "The counter's value is 1 before method decrement executes, so the value should be 0 after it has executed.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#decrement()}.
+	 */
+	@Test
+	void testDecrementFromZeroToNegative() {
+		Counter counter = new Counter();
+        counter._ctr = 0;
+       
+        counter.decrement();
+
+        assertEquals(-1, counter._ctr, "The counter's value is 0 before method decrement executes, so the value should be -1 after it has executed.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#decrement()}.
+	 */
+	@Test
+	void testDecrementFromNegativeToNegative() {
+		Counter counter = new Counter();
+        counter._ctr = -1;
+       
+        counter.decrement();
+
+        assertEquals(-2, counter._ctr, "The counter's value is -1 before method decrement executes, so the value should be -2 after it has executed.");
 	}
 
 	/**
 	 * Test method for {@link se283.a1.t1.Counter#reset()}.
 	 */
 	@Test
-	void testReset() {
-		fail("Not yet implemented");
+	void testResetFromNegative() {
+		Counter counter = new Counter();
+        counter._ctr = -1;        
+
+        counter.reset();
+
+        assertEquals(0, counter._ctr, "The counter's value should be 0 after having executed method reset.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#reset()}.
+	 */
+	@Test
+	void testResetFromZero() {
+		Counter counter = new Counter();
+        counter._ctr = 0;        
+
+        counter.reset();
+
+        assertEquals(0, counter._ctr, "The counter's value should be 0 after having executed method reset.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#reset()}.
+	 */
+	@Test
+	void testResetFromPositive() {
+		Counter counter = new Counter();
+        counter._ctr = 1;
+        
+        counter.reset();
+
+        assertEquals(0, counter._ctr, "The counter's value should be 0 after having executed method reset.");
 	}
 
 	/**
 	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
 	 */
 	@Test
-	void testMultiplyBy() {
-		fail("Not yet implemented");
+	void testMultiplyByNegativeAndNegative() {
+		Counter counter = new Counter();         
+        counter._ctr = -1;
+        
+        counter.multiplyBy(-1);
+
+        assertEquals(1, counter._ctr, "The counter's value is -1 before method multiplyBy executes with argument -1, so the value should be 1 after the method has done so.");
 	}
 
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByNegativeAndZero() {
+		Counter counter = new Counter();         
+        counter._ctr = -1;
+        
+        counter.multiplyBy(0);
+
+        assertEquals(0, counter._ctr, "The counter's value is -1 before method multiplyBy executes with argument 0, so the value should be 0 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByNegativeAndPositive() {
+		Counter counter = new Counter();         
+        counter._ctr = -1;
+        
+        counter.multiplyBy(1);
+
+        assertEquals(-1, counter._ctr, "The counter's value is -1 before method multiplyBy executes with argument 1, so the value should be -1 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByZeroAndNegative() {
+		Counter counter = new Counter();         
+        counter._ctr = 0;
+        
+        counter.multiplyBy(-1);
+
+        assertEquals(0, counter._ctr, "The counter's value is 0 before method multiplyBy executes with argument -1, so the value should be 0 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByZeroAndZero() {
+		Counter counter = new Counter();         
+        counter._ctr = 0;
+        
+        counter.multiplyBy(0);
+
+        assertEquals(0, counter._ctr, "The counter's value is 0 before method multiplyBy executes with argument 0, so the value should be 0 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByZeroAndPositive() {
+		Counter counter = new Counter();         
+        counter._ctr = 0;
+        
+        counter.multiplyBy(1);
+
+        assertEquals(0, counter._ctr, "The counter's value is 0 before method multiplyBy executes with argument 1, so the value should be 0 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByPositiveAndNegative() {
+		Counter counter = new Counter();         
+        counter._ctr = 1;
+        
+        counter.multiplyBy(-1);
+
+        assertEquals(-1, counter._ctr, "The counter's value is 1 before method multiplyBy executes with argument -1, so the value should be -1 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByPositiveAndZero() {
+		Counter counter = new Counter();         
+        counter._ctr = 1;
+        
+        counter.multiplyBy(0);
+
+        assertEquals(0, counter._ctr, "The counter's value is 1 before method multiplyBy executes with argument 0, so the value should be 0 after the method has done so.");
+	}
+
+	/**
+	 * Test method for {@link se283.a1.t1.Counter#multiplyBy(int)}.
+	 */
+	@Test
+	void testMultiplyByPositiveAndPositive() {
+		Counter counter = new Counter();         
+        counter._ctr = 1;
+        
+        counter.multiplyBy(1);
+
+        assertEquals(1, counter._ctr, "The counter's value is 1 before method multiplyBy executes with argument 1, so the value should be 1 after the method has done so.");
+	}
 }
