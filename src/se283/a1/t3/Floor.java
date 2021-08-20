ab80f34c733f2eae727b1dd46c5490c072d3168e
Gen Li <gli@aucklanduni.ac.nz>
2021-08-20 20:03:02 +0900

    docs, feat, refactor, style
    Changed class Floor's class comment, removed comments inside some methods of class Floor, added method getArea to class Floor, renamed some methods of class Floor, and removed empty lines from file Floor.java.
    
    - Added a class description, a version date, and descriptions of the changes made to class Floor to class Floor's class comment.
    - Removed comments inside methods length and width of class Floor: they're non-essential.
    - Added method getArea to class Floor: it's likely that it'll be useful.
    - Removed empty lines from file Floor.java: they're non-essential.

--- src/se283/a1/t3/Floor.java
+++ src/se283/a1/t3/Floor.java
@@ -1,33 +1,34 @@
 package se283.a1.t3;
 
 /**
- * [ADD COMMENTS] Describe the class
+ * It represents a floor.
  * 
  * @author Author Name: [YOUR NAME] Author UPI: [YOUR UPI]
- * @version Date: [CURRENT DATE] 
+ * @version Date: 20/08/2021
  *
- * [Explain the changes made and their rationale. 
- * This description may overlap with the contents of commit messages]
+ * - Removed comments inside methods length and width: they're non-essential.
+ * - Renamed methods length and width to getLength and getWidth respectively: it's for method-name consistency.
+ * - Added method getArea: it's likely that it'll be useful.
+ * - Removed empty lines: they're non-essential.
  * 
  * Note: You may create new classes, methods or fields in this package
  *
  */
-
 public class Floor {
-
-	public int length() {
-		// length of the floor
+	public int getLength() {
 		return 21;
 	}
 
-	public int width() {
-		// width of the floor
+	public int getWidth() {
 		return 45;
 	}
+
+    public int getArea() {
+        return getLength()*getWidth();
+    }
 
 	public int getCondition() {
 		// Current condition of the floor rated on the scale of 1 (worst) to 10 (best)
 		return 5;
 	}
-
 }
