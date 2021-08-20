6fcfc522efea15ef73b799f8372e3b5257703ef4
Gen Li <gli@aucklanduni.ac.nz>
2021-08-20 21:35:38 +0900

    docs, style
    Changed the class comments of classes CleaningMachine and PolishingMachine, and added @Override annotations to the classes' methods.
    
    - Added a description of the changes made to class CleaningMachine to the class's class comment, and did so for class PolishingMachine.
    - Added @Override annotations to the methods of classes CleaningMachine and PolishingMachine; it's a style preference.

--- src/se283/a1/t3/CleaningMachine.java
+++ src/se283/a1/t3/CleaningMachine.java
@@ -10,15 +10,18 @@ package se283.a1.t3;
  * - Renamed method costPerUnitElectricity to getCostPerUnitElectricity; it's for method-name consistency.
  * - Moved comments inside the methods to abstract-class Machine: it's for comment minimisation.
  * - Removed empty lines: they're non-essential.
+ * - Added @Override annotations to methods getCapability and getCostPerUnitElectricity; it's a style preference.
  *
  * Note: You may create new classes, methods or fields in this package
  *
  */
 public class CleaningMachine extends Machine {
+    @Override
 	public int getCapability() {
 		return 65; 
 	}
 
+    @Override
 	public double getCostPerUnitElectricity() {
 		return 4.0;
 	}
